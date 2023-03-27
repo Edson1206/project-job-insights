d4052d230856a0dd776a1aa87e4fd00f010fb7e2
Edson Pereira <ed.1206@gmail.com>
2023-03-27 19:01:16 -0300

    feat: :sparkles: Implemented matches salary range function

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -71,7 +71,18 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    raise NotImplementedError
+    value = int(salary) if isinstance(salary, str) else salary
+
+    if ("min_salary" not in job or "max_salary" not in job):
+        raise ValueError("'min_salary' or 'max_salary' doesn't exist.")
+
+    elif (type(job["min_salary"]) != int or type(job["max_salary"]) != int):
+        raise ValueError("'min_salary' or 'max_salary' aren't valid integers.")
+
+    elif (job["min_salary"] > job["max_salary"]):
+        raise ValueError("'min_salary' is greater than 'max_salary'.")
+
+    return True if job["min_salary"] <= value <= job["max_salary"] else False
 
 
 def filter_by_salary_range(
