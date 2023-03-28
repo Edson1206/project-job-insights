7ff108363056238346d11a7bbb7a0ca2be5116f0
Edson Pereira <ed.1206@gmail.com>
2023-03-27 23:40:50 -0300

    test: :white_check_mark: Test for count ocurrences function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    javascript = count_ocurrences("data/jobs.csv", "JavaScript")
+    python = count_ocurrences("data/jobs.csv", "Python")
+
+    assert javascript == 122
+    assert python == 1639
